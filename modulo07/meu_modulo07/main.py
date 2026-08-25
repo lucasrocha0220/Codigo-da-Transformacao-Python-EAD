1460364abb305d1e7b4558594f59f176109a499f
lucas <lucasrochade8@gmail.com>
2026-08-25 16:40:40 -0300

    Update main.py

---
+++ modulo07/meu_modulo07/main.py
@@ -0,0 +1,3 @@
+#o ultimo projeto vai ser organizado em um arquivo chamado main.py,
+##que vai importar as funções dos outros arquivos e vai poder exportar as informaçoes para o usuário, ou seja
+# , vai ser a interface do usuário, onde ele vai interagir com o programa.
